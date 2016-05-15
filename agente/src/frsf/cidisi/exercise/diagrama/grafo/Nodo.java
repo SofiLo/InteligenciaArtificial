da8b9bb5353c2a5fa60ef1d93024899f898dc36f
franechavarria <francisco@echavarria.com.ar>
2016-05-15 19:03:11 -0300

    Commit de Fran

--- agente/src/frsf/cidisi/exercise/diagrama/grafo/Nodo.java
+++ agente/src/frsf/cidisi/exercise/diagrama/grafo/Nodo.java
@@ -16,6 +16,13 @@ public class Nodo {
 		this.nivel = nivel;
 		this.obstaculo = obstaculo;*/
 	}
+	public Nodo(String nombre, int idNodo, int nivel, boolean obstaculo) {
+		super();
+		this.nombre = nombre;
+		this.idNodo = idNodo;
+		this.nivel = nivel;
+		this.obstaculo = obstaculo;
+	}
 	public int getIdNodo() {
 		return idNodo;
 	}
--- agente/src/frsf/cidisi/exercise/diagrama/search/EstadoAgente.java
+++ agente/src/frsf/cidisi/exercise/diagrama/search/EstadoAgente.java
@@ -1,7 +1,6 @@
 package frsf.cidisi.exercise.diagrama.search;
 
-import java.util.List;
-
+import frsf.cidisi.exercise.diagrama.grafo.Grafo;
 import frsf.cidisi.exercise.diagrama.grafo.Nodo;
 import frsf.cidisi.faia.agent.Perception;
 import frsf.cidisi.faia.agent.search.SearchBasedAgentState;
@@ -10,103 +9,98 @@ import frsf.cidisi.faia.agent.search.SearchBasedAgentState;
  * Represent the internal state of the Agent.
  */
 public class EstadoAgente extends SearchBasedAgentState {
-	
-    public EstadoAgente() {
-    
-    	//TODO: Complete Method
-    	/*
-			// posicion = initData0;
-			// energia = initData1;
-			// listaObstaculo = initData2;
-			// nodoDestino = initData3;
-        */
-    	
-        this.initState();
-    }
 
-    /**
-     * This method clones the state of the agent. It's used in the search
-     * process, when creating the search tree.
-     */
-    @Override
-    public SearchBasedAgentState clone() {
-        
-		//TODO: Complete Method
-		
-        return null;
-    }
+	private int energia;
+	private Nodo posicion;
+	private Grafo mapaAgente; //Lista de obstaculos
+	private Nodo nodoDestino;
 
-    /**
-     * This method is used to update the Agent State when a Perception is
-     * received by the Simulator.
-     */
-    @Override
-    public void updateState(Perception p) {
-        
-        //TODO: Complete Method
-    }
+	public EstadoAgente() {
+		this.initState();
+	}
 
-    /**
-     * This method is optional, and sets the initial state of the agent.
-     */
-    @Override
-    public void initState() {
-        
-	//TODO: Complete Method
+	/**
+	 * This method clones the state of the agent. It's used in the search
+	 * process, when creating the search tree.
+	 */
+	@Override
+	public SearchBasedAgentState clone() {
 
-    }
+		// TODO: Complete Method
 
-    /**
-     * This method returns the String representation of the agent state.
-     */
-    @Override
-    public String toString() {
-        String str = "";
+		return null;
+	}
 
-        //TODO: Complete Method
+	/**
+	 * This method is used to update the Agent State when a Perception is
+	 * received by the Simulator.
+	 */
+	@Override
+	public void updateState(Perception p) {
 
-        return str;
-    }
+		// TODO: Complete Method
+	}
 
-    /**
-     * This method is used in the search process to verify if the node already
-     * exists in the actual search.
-     */
-    @Override
-    public boolean equals(Object obj) {
-       
-       //TODO: Complete Method
-        
-        return true;
-    }
+	/**
+	 * This method is optional, and sets the initial state of the agent.
+	 */
+	@Override
+	public void initState() {
+		energia = 1;
+		mapaAgente = new Grafo();
+		posicion = new Nodo("Mastil", 0, 0, false);
+	}
 
-    //TODO: Complete this section with agent-specific methods
-    // The following methods are agent-specific:
-   	
-//     public Other getposicion(){
-//        return posicion;
-//     }
-//     public void setposicion(Other arg){
-//        posicion = arg;
-//     }
-//     public int getenergia(){
-//        return energia;
-//     }
-//     public void setenergia(int arg){
-//        energia = arg;
-//     }
-//     public Other getlistaObstaculo(){
-//        return listaObstaculo;
-//     }
-//     public void setlistaObstaculo(Other arg){
-//        listaObstaculo = arg;
-//     }
-//     public Other getnodoDestino(){
-//        return nodoDestino;
-//     }
-//     public void setnodoDestino(Other arg){
-//        nodoDestino = arg;
-//     }
-	
+	/**
+	 * This method returns the String representation of the agent state.
+	 */
+	@Override
+	public String toString() {
+		String str = "";
+
+		// TODO: Complete Method
+
+		return str;
+	}
+
+	/**
+	 * This method is used in the search process to verify if the node already
+	 * exists in the actual search.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+
+		// TODO: Complete Method
+
+		return true;
+	}
+
+	// TODO: Complete this section with agent-specific methods
+	// The following methods are agent-specific:
+
+	// public Other getposicion(){
+	// return posicion;
+	// }
+	// public void setposicion(Other arg){
+	// posicion = arg;
+	// }
+	// public int getenergia(){
+	// return energia;
+	// }
+	// public void setenergia(int arg){
+	// energia = arg;
+	// }
+	// public Other getlistaObstaculo(){
+	// return listaObstaculo;
+	// }
+	// public void setlistaObstaculo(Other arg){
+	// listaObstaculo = arg;
+	// }
+	// public Other getnodoDestino(){
+	// return nodoDestino;
+	// }
+	// public void setnodoDestino(Other arg){
+	// nodoDestino = arg;
+	// }
+
 }
-
